808eaeaad08f75496978d2375149232444ed755f
Kvj-Harsha <cs23b1034@iiitr.ac.in>
2024-12-20 18:29:56 +0530

    Clean Up - Added few dependencies, removed unecessary files

--- app/About/page.tsx
+++ app/About/page.tsx
@@ -3,12 +3,16 @@ import HeroAbout from '../_components/HeroAbout'
 import Navbar from '../_components/Navbar'
 
 function page() {
+
   return (
+    
     <div>
+
         <Navbar/>
       <HeroAbout/>
       
     </div>
+
   )
 }
 
